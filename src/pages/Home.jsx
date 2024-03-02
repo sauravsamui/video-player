5091f30d1127c7c764cab9ab8b875e6399b6be8e
Saurav Samui <samuisaurav1@gmail.com>
2024-03-02 18:31:12 +0530

    tailwind configure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
